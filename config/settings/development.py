8c7eddbd0a021804661d6a1508ca0fce78e147aa
AmeyaJain-25 <ameiya.jain@gmail.com>
2024-07-29 20:29:49 +0530

    allow ec2 ip address

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'http://ec2-15-206-67-159.ap-south-1.compute.amazonaws.com' ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
